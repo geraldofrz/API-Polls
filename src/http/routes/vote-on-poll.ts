83d1a658aaa88c27ca44f9f24f945d569720750c
Geraldo Ibere Frazao <geraldoneto39@gmail.com>
2024-02-08 15:10:12 -0300

    vote on poll route

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,64 @@
+import { z } from "zod";
+import { randomUUID } from 'node:crypto';
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (req, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = voteOnPollParams.parse(req.params)
+    const { pollOptionId } = voteOnPollBody.parse(req.body)
+
+
+    let { sessionId } = req.cookies
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        }
+      })
+
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          },
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({ message: 'You already voted on this poll.' })
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      })
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      }
+    })
+
+    return reply.status(201).send();
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { createPoll } from './routes/create-poll'
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "polls-app-user",
+  hook: 'onRequest',
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
