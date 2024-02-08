a0eb510170ce88e0d80fff7bfe5b38412044782a
Geraldo Ibere Frazao <geraldoneto39@gmail.com>
2024-02-08 11:12:59 -0300

    get poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { createPoll } from './routes/create-poll'
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
